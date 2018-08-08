533d3a14b9a0c80cc63008e632a047ec3cfa85e1
Martin Croome <martin.croome@greenwaves-technologies.com>
2018-08-08 19:11:37 +0200

    more modifications to ftdi find to make windows easier

--- cmake/FindFTDI.cmake
+++ cmake/FindFTDI.cmake
@@ -6,36 +6,56 @@
 #  FTDI_INCLUDE_DIR - ~ the ftdi include directory 
 #  FTDI_LIBRARY - Link these to use ftdi
 
-FIND_PATH(FTDI_INCLUDE_DIR 
-NAMES   ftdi.h
-PATHS   /usr/local/include
-        /usr/include
-    /opt/local/include
-    /libftdi/include
-PATH_SUFFIXES libftdi libftdi1
-)
+include( SelectLibraryConfigurations )
+include( FindPackageHandleStandardArgs )
 
-FIND_LIBRARY(FTDI_LIBRARY
-NAMES ftdi ftdi1
-PATHS /usr/lib
+if (FTDI_INCLUDE_DIR AND FTDI_LIBRARIES)
+
+  # in cache already
+  set(FTDI_FOUND TRUE)
+
+else (FTDI_INCLUDE_DIR AND FTDI_LIBRARIES)
+  if(NOT WIN32)
+    find_package(PkgConfig REQUIRED)
+    pkg_check_modules(FDTI libftdi)
+    set(FTDI_VERSION ${PC_FTDI_VERSION})
+  endif(NOT WIN32)
+
+  find_path(FTDI_INCLUDE_DIR
+    NAMES ftdi.h
+    PATHS
+      ${PC_FDTI_INCLUDE_DIRS}
+      /usr/local/include
+      /usr/include
+      ${GNUWIN32_DIR}/include
+      $ENV{FTDI_INCLUDE_DIR}
+      PATH_SUFFIXES ftdi
+  )
+
+  find_library(FTDI_LIBRARY
+    NAMES ftdi ftdi1
+    PATHS
+      ${PC_FDTI_INCLUDE_DIRS}
+      /usr/lib
       /usr/local/lib
-      /opt/local/lib
-      /libftdi/lib64
-)
+      ${GNUWIN32_DIR}/lib
+      $ENV{FTDI_LIBRARY}
+  )
 
-IF (FTDI_LIBRARY)
-  IF(FTDI_INCLUDE_DIR)
-    set(FTDI_FOUND TRUE)
-    MESSAGE(STATUS "Found libFTDI: ${FTDI_INCLUDE_DIR}, ${FTDI_LIBRARY}")
-  ELSE(FTDI_INCLUDE_DIR)
-    set(FTDI_FOUND FALSE)
-    MESSAGE(STATUS "libFTDI headers NOT FOUND. Make sure to install the development headers!")
-  ENDIF(FTDI_INCLUDE_DIR)
-ELSE (FTDI_LIBRARY)
-    set(FTDI_FOUND FALSE)
-    MESSAGE(STATUS "libFTDI NOT FOUND.")
-ENDIF (FTDI_LIBRARY)
+  find_package_handle_standard_args(FTDI
+    FOUND_VAR FTDI_FOUND
+    REQUIRED_VARS
+      FTDI_LIBRARY
+      FTDI_INCLUDE_DIR
+    VERSION_VAR FTDI_VERSION
+  )
 
-set(FTDI_INCLUDE_DIR
-    ${FTDI_INCLUDE_DIR}
-)
+  if(FTDI_FOUND)
+    set(FTDI_LIBRARIES ${FTDI_LIBRARY})
+    set(FTDI_INCLUDE_DIRS ${FTDI_INCLUDE_DIR})
+    set(FDTI_DEFINITIONS ${PC_FTDI_CFLAGS_OTHER})
+  endif()
+
+  mark_as_advanced(FTDI_INCLUDE_DIR FTDI_LIBRARY)
+
+endif (FTDI_INCLUDE_DIR AND FTDI_LIBRARIES)
---
+++ cmake/ftdi-config.cmake
@@ -0,0 +1,1 @@
+FindFTDI.cmake
